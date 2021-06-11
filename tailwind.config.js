81f89069a9d30439d654ae3a02485480fc7d32af
GKuzin13 <yo.is.josh@gmail.com>
2021-06-12 02:03:23 +0300

    Added styling with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+      },
+      backgroundImage: (theme) => ({
+        main: 'radial-gradient( circle 993px at 0.5% 50.5%,  rgba(137,171,245,0.37) 0%, rgba(245,247,252,1) 100.2% )',
+      }),
+      zIndex: {
+        '-10': '-10',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
